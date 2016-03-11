4604b97d83c48782dfec33dfca3307e8b3241a4f
annokr <annokr1@gmail.com>
2016-03-11 15:39:36 +0100

    change navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,9 @@
-module.export = $(window).scroll(function() {
-    if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/
-    {
-        $('.opaque-navbar').addClass('opaque');
-    } else {
-        $('.opaque-navbar').removeClass('opaque');
-    }
-});
+module.export = {
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 50) /*height in pixels when the navbar becomes non opaque*/ {
+			$('.opaque-navbar').addClass('opaque');
+		} else {
+			$('.opaque-navbar').removeClass('opaque');
+		}
+	});
+}
