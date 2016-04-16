5c02bc22966b3a0c9c5058f2244b8fd2d74039d8
annokr <annokr1@gmail.com>
2016-04-16 15:24:40 +0200

    Add navbar scroll

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,13 @@
-module.export = {
-	$(window).scroll(function() {
-		if ($(this).scrollTop() > 50) /*height in pixels when the navbar becomes non opaque*/ {
-			$('.opaque-navbar').addClass('opaque');
-		} else {
-			$('.opaque-navbar').removeClass('opaque');
-		}
-	});
-}
+$ = jQuery;
+
+module.exports = {
+	init: function() {
+		$(window).scroll(function() {
+			if ($(this).scrollTop() > 50) /*height in pixels when the navbar becomes non opaque*/ {
+				$('.opaque-navbar').addClass('opaque');
+			} else {
+				$('.opaque-navbar').removeClass('opaque');
+			}
+		});
+	}
+};
